0d047aa257166647ee96c96003a4b49c79a4e37e
Romeao Jennings <romeao.jennings@gmail.com>
2019-01-13 12:03:44 -0800

    Add core module with home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationModule } from './navigation/navigation.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NavigationModule],
+  imports: [BrowserModule, AppRoutingModule, NavigationModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })
