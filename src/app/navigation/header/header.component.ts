0923f1b8c49cbd6cae0f9b62e68765e1b76de2d7
Romeao Jennings <romeao.jennings@gmail.com>
2019-01-14 11:07:35 -0800

    Add and format header and navbar

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faUserCircle } from '@fortawesome/free-regular-svg-icons';
+import {
+  faShoppingBag,
+  faSearch,
+  faBars,
+  faWindowClose
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  // Font Awesome Icons
+  myAccountIcon = faUserCircle;
+  myBagIcon = faShoppingBag;
+  searchIcon = faSearch;
+  hamburgerIcon = faBars;
 
-  constructor() { }
+  navBarCollapsed = true;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onToggleHamburger() {
+    this.navBarCollapsed = !this.navBarCollapsed;
+    this.hamburgerIcon = this.hamburgerIcon === faBars ? faWindowClose : faBars;
   }
-
 }
--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  declarations: [HeaderComponent, FooterComponent, NavbarComponent],
+  imports: [CommonModule, FontAwesomeModule],
   exports: [HeaderComponent, FooterComponent]
 })
 export class NavigationModule {}
