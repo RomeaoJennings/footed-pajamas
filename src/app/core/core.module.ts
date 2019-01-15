7f5eb607a8713e2933758f8c23260a0b1dfa095b
Romeao Jennings <romeao.jennings@gmail.com>
2019-01-14 18:18:22 -0800

    add shells for product and product list components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product-list/product/product.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ProductListComponent, ProductComponent],
   imports: [CommonModule]
 })
 export class CoreModule {}
