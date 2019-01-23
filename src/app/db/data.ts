584845c942d319f5441b38fc817766821fb2a2ca
Romeao Jennings <romeao.jennings@gmail.com>
2019-01-23 15:21:28 -0800

    Add data from new format for products

---
+++ src/app/db/data.ts
@@ -0,0 +1,28 @@
+export const products = [
+  {
+    id: 'CH-801-H',
+    name: 'In the Clouds {Category} Chenille Onesie',
+    features: ['footed', 'hoodie'],
+    colors: ['white'],
+    washInstructions:
+      'Warm water on a delicate cycle, tumble dry with warm heat',
+    notes: [
+      'Super soft and cuddily',
+      'Provides warmth without weighing a lot',
+      'Unisex',
+      'Versatile (lounge, layer, sleep, play, etc.)'
+    ]
+  }
+];
+
+export const details = {
+  productId: 'CH-801-H',
+  category: 'infant',
+  listPrice: '24.99',
+  salePrice: '17.99',
+  imageUrls: [
+    'CH-801-H%2Finfant%2FCH-801-IH-1.jpg',
+    'CH-801-H%2Finfant%2FCH-801-IH-2.jpg',
+    'CH-801-H%2Finfant%2FCH-801-IH-3.jpg'
+  ]
+};
