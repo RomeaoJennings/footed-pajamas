38c8bf0e0ef147ece75f2e197c782b5129e1bfa0
Romeao Jennings <romeao.jennings@gmail.com>
2019-01-26 14:19:57 -0800

    Refactor effects into product module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { ProductService } from './shared/product.service';
-import { environment } from '../environments/environment';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import { reducers } from './store/app.reducers';
-import { ProductEffects } from './products/store/product.effects';
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 import { ProductsModule } from './products/products.module';
+import { AppComponent } from './app.component';
+import { ProductService } from './shared/product.service';
+
+import { reducers } from './store/app.reducers';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     environment.production ? [] : StoreDevtoolsModule.instrument(),
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule,
-    AngularFireStorageModule,
     AppRoutingModule,
     CoreModule,
     ProductsModule,
     StoreModule.forRoot(reducers),
-    EffectsModule.forRoot([ProductEffects])
+    EffectsModule.forRoot([])
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EffectsModule } from '@ngrx/effects';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
+import { environment } from 'src/environments/environment';
 import { ProductsContainerComponent } from './products.container';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductListRowComponent } from './product-list/product-list-row/product-list-row.component';
+import { ProductEffects } from './store/product.effects';
 
 @NgModule({
   declarations: [
@@ -11,6 +16,11 @@ import { ProductListRowComponent } from './product-list/product-list-row/product
     ProductListComponent,
     ProductListRowComponent
   ],
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    EffectsModule.forFeature([ProductEffects])
+  ]
 })
 export class ProductsModule {}
