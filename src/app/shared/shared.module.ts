0248cc7d5a01c20026f8a8a81d711337bd1a35ba
Romeao Jennings <romeao.jennings@gmail.com>
2019-01-28 09:56:53 -0800

    Remove ProductService from declarations

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [],
-  providers: [ProductService],
   imports: [CommonModule]
 })
 export class SharedModule {}
