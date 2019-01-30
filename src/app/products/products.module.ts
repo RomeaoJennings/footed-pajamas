d9b6189fe28c157556db39787e3039c0ff63697f
Romeao Jennings <romeao.jennings@gmail.com>
2019-01-29 19:44:28 -0800

    Update product header with mock text for development

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -9,7 +9,7 @@ import { ProductsRowComponent } from './products-row/products-row.component';
 import { ProductsComponent } from './products.component';
 import { ProductDataResolver } from './products.dataResolver';
 import { WindowProvider } from './services/window-provider.service';
-import { FiltersContainerComponent } from './filters/filters.container.';
+import { FiltersContainerComponent } from './filters/filters.container';
 import { ProductHeaderComponent } from './product-header/product-header.component';
 
 @NgModule({
